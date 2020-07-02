1b600aff5ec1422df4872a6151c52a7a00e6eb24
Maryia Brodnikava <maryia_brodnikava@epam.com>
2020-07-02 19:52:11 +0300

    persisted redux state to local storage (#18)

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router'
 import { createBrowserHistory } from 'history';
+import throttle from 'lodash/throttle';
 import createStore from './store/createStore';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import { loadState, saveState } from './utils/localStorage';
 
 const appHistory = createBrowserHistory();
-const store = createStore(appHistory, {});
+const persistedState = loadState();
+const store = createStore(appHistory, persistedState);
+
+store.subscribe(throttle(() => {
+  saveState({
+    userAccount: store.getState().userAccount,
+  });
+}), 1000);
 
 ReactDOM.render(
   <Provider store={store}>
