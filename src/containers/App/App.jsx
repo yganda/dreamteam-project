43c37abb6764f33185f109ca265d32a0b126cf8e
GOMEL\Maryia_Brodnikava <Maryia_Brodnikava@epam.com>
2020-07-02 21:58:38 +0300

    added homepage

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
     <div className="app">
       <MainHeader/>
         <Switch>
-          <Route exact path="/" render={ props => <Home { ...props } /> } />
+          <Route exact path="/dreamteam-project" render={ props => <Home { ...props } /> } />
           <Route exact path="/positions" render={ props => <PositionSearch { ...props } /> } />
           <Route exact path="/positions/:positionId" render={ props => <Position { ...props } /> } />
           <Route exact path="/projects/:projectId" render={ props => <Project { ...props } /> } />
