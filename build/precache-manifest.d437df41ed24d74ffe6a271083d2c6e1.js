a23ac2b85e75214748c6f7b4d88325ca6b51581f
Andrei Furs <andrei_furs@epam.com>
2020-07-02 18:44:36 +0300

    added build

---
+++ build/precache-manifest.d437df41ed24d74ffe6a271083d2c6e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67719ddf15e7a1c3dc06dbf4f4a0cf09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d14fc6ed67adf6f8b50",
+    "url": "/static/css/main.ad1333da.chunk.css"
+  },
+  {
+    "revision": "dcebc68a1b3aec2a1742",
+    "url": "/static/js/2.cdb11f3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cdb11f3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d14fc6ed67adf6f8b50",
+    "url": "/static/js/main.564fba09.chunk.js"
+  },
+  {
+    "revision": "df704516c463411b78bd",
+    "url": "/static/js/runtime-main.fd6abcd4.js"
+  }
+]);
