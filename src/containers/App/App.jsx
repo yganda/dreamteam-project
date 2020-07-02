e0f2ad1a9c31c94f7f073458e14e5f04f4ea5812
Maryia Brodnikava <maryia_brodnikava@epam.com>
2020-07-02 22:04:19 +0300

    404page (#20)
    * created not found page
    
    * created not found page
    
    * added homepage

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.scss';
 import MainHeader from '../../components/MainHeader';
-import ModalContainer from '../../components/ModalContainer';
 import Home from '../Home';
+import ModalContainer from '../../components/ModalContainer';
+import NotFoundPage from '../../containers/NotFoundPage';
 import PositionSearch from '../PositionSearch';
 import Position from '../Position';
 import Project from '../Project';
@@ -13,10 +14,11 @@ const App = () => {
     <div className="app">
       <MainHeader/>
         <Switch>
-          <Route exact path="/" render={ props => <Home { ...props } /> } />
+          <Route exact path="/dreamteam-project" render={ props => <Home { ...props } /> } />
           <Route exact path="/positions" render={ props => <PositionSearch { ...props } /> } />
           <Route exact path="/positions/:positionId" render={ props => <Position { ...props } /> } />
           <Route exact path="/projects/:projectId" render={ props => <Project { ...props } /> } />
+          <Route path="*" render={ props => <NotFoundPage { ...props } /> } />
         </Switch>
       <ModalContainer />
     </div>
