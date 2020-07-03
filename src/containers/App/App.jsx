e698bd6d6212a07127f4373630e5e90735d210d0
YuliaBarbashova <45540461+YuliaBarbashova@users.noreply.github.com>
2020-07-03 15:12:14 +0300

    19 under construction (#31)
    * 18 fix dropdown, added logo, added redirect to the Home page by clicking on logo
    
    * add underconstruction page
    
    * 19 added create project page/ underConstruction

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -8,6 +8,7 @@ import NotFoundPage from '../../containers/NotFoundPage';
 import PositionSearch from '../PositionSearch';
 import Position from '../Position';
 import Project from '../Project';
+import CreateProject from '../CreateProject';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
           <Route exact path="/positions" render={ props => <PositionSearch { ...props } /> } />
           <Route exact path="/positions/:positionId" render={ props => <Position { ...props } /> } />
           <Route exact path="/projects/:projectId" render={ props => <Project { ...props } /> } />
+          <Route exact path="/post-project" render={ props => <CreateProject { ...props } /> } />
           <Route path="*" render={ props => <NotFoundPage { ...props } /> } />
         </Switch>
       <ModalContainer />
