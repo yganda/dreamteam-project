e375b0c052f4b36226a5c34fabe646b4459898bf
Yulia Barbashova <yulia_barbashova@epam.com>
2020-07-03 13:50:38 +0300

    19 added create project page/ underConstruction

--- src/containers/CreateProject/CreateProject.js
+++ src/containers/CreateProject/CreateProject.js
@@ -1,11 +1,17 @@
 import React from "react";
-import './CreateProject.scss';
+import { ReactComponent as UnderConstructionImage } from "../../assets/underConstruction-icon.svg";
+import { Link } from "react-router-dom";
+import "./CreateProject.scss";
 
 export const CreateProject = () => {
   return (
     <div className="createProject-wrapper">
       <div className="createProject">
-          <div className="createProject-title">Sorry! <br/> This page is under construction.</div>
+        <div className="createProject-title">
+          <Link to={"/dreamteam-project"}>
+            <UnderConstructionImage />
+          </Link>
+        </div>
       </div>
     </div>
   );
